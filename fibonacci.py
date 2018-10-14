3ea04b93331b695bf4a0f2bf9decb43ab8e8c2a1
tesladodger <andrerfmartins@protonmail.com>
2018-10-14 18:51:53 +0100

    v1.6
    Just code style consistency, nothing to update

--- fibonacci.py
+++ fibonacci.py
@@ -7,19 +7,19 @@ import os
 
 def ntimes(n) :
 	save_file = 0;
-	try:
+	try :
 		n = int(n);
-	except ValueError:
+	except ValueError :
 		print('\n\nYou must introduce an integer\n\n')
 		return save_file;
-	if (n <= 0):
+	if (n <= 0) :
 		print('\n\nYou must introduce a positive integer\n\n')
 		return save_file; #=False
 	os.system('cls')
 	num = 1;
 	next_num = 1;
 	print(num)
-	for x in range(0,n-1):         
+	for x in range(0,n-1) :         
 		print(next_num)            #By doing this we do one less calculation
 		temp = next_num;           #Is there a better way to do this without temp?
 		next_num = next_num + num;
@@ -27,21 +27,21 @@ def ntimes(n) :
 	print('\nS    - Save to a file')
 	print('else - Go to the main menu')
 	ntimeschoice = str(input('==> '));
-	if (ntimeschoice=='s'):
+	if (ntimeschoice=='s') :
 		save_file = 1;
 		return save_file; #=True
-	else:
+	else :
 		os.system('cls')
 		return save_file; #=False
 
 
 def upton(maxnum) :
-	try:
+	try :
 		maxnum = int(maxnum);
-	except ValueError:
+	except ValueError :
 		print('\n\nYou must introduce an integer\n\n')
 		return;
-	if (maxnum < 1):
+	if (maxnum < 1) :
 		print('\n\nYou must insert a value greater or equal to 1\n\n')
 	os.system('cls')
 	num = 1;
@@ -57,9 +57,9 @@ def upton(maxnum) :
 	print('\nS    - Show more information')
 	print('else - Go to the main menu')
 	ntimeschoice = str(input('==> '));
-	if (ntimeschoice=='s'):
+	if (ntimeschoice=='s') :
 		os.system('cls')
-		if (num==maxnum):
+		if (num==maxnum) :
 			print("The number", maxnum, "is in the Fibonacci sequence")
 		print("Number of iterations:       ", itera)
 		print("Your number:                ", maxnum)
@@ -67,7 +67,7 @@ def upton(maxnum) :
 		print("Diference to that number:   ", (next_num - maxnum))
 		print("\n\n")
 		return;
-	else:
+	else :
 		print('')
 		return;
 
@@ -82,7 +82,7 @@ def file_saver(n) :
 	num = 1;
 	next_num = 1;
 	n = int(n); #No need to check, we already know it's an int
-	for x in range(0,n):
+	for x in range(0,n) :
 		str_num = str(num) + '\n';
 		file.write(str_num)
 		temp = next_num;
@@ -108,15 +108,15 @@ while (repeat==True) :
 	if (choice=='1') :
 		n = str(input('Number of times: '));
 		save_file = ntimes(n);
-		if (save_file==True):
+		if (save_file==True) :
 			name = file_saver(n);
 			os.system('cls')
 			print('\nFile saved as:', name, '\n\n')
-	elif (choice=='2'):
+	elif (choice=='2') :
 		maxnum = str(input('Calculate to the number: '))
 		upton(maxnum);
-	elif (choice=='T'):
+	elif (choice=='T') :
 		print('\nGoodbye\n')
 		repeat = 0;
-	else:
+	else :
 		print('\nInvalid option\n')
